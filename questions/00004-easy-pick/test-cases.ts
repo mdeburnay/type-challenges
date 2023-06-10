bee0d967e32dcd390054841a573639470fea9438
Max De Burnay-Bastos <de_burnay@hotmail.co.uk>
2023-06-10 23:02:32 +0100

    first challenged, copied youtube lol need to go again

--- questions/00004-easy-pick/test-cases.ts
+++ questions/00004-easy-pick/test-cases.ts
@@ -1,10 +1,16 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+/* eslint-disable @typescript-eslint/comma-dangle */
+/* eslint-disable @typescript-eslint/quotes */
+import type { Equal, Expect } from "@type-challenges/utils"
+
+type MyPick<TObj, K extends keyof TObj> = {
+  [SpecificKey in K]: TObj[SpecificKey]
+}
 
 type cases = [
-  Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
-  Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
+  Expect<Equal<Expected1, MyPick<Todo, "title">>>,
+  Expect<Equal<Expected2, MyPick<Todo, "title" | "completed">>>,
   // @ts-expect-error
-  MyPick<Todo, 'title' | 'completed' | 'invalid'>,
+  MyPick<Todo, "title" | "completed" | "invalid">
 ]
 
 interface Todo {
